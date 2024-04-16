18b0f144f94db43a12d98a50cc680a9d1d536bc4
eshansinghal14 <eshansinghal05@gmail.com>
2024-04-16 16:37:13 -0400

    ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import Hello from "./components/Hello";
 function App() {
   return (
     <div className="App">
-      <h1>Starter Extension</h1>
+      <h1>Extension Name</h1>
       {/* Render the SnippetList component with the snippets and event handlers */}
-      <Hello person="World" />
+      <Hello person="Wod" />
     </div>
   );
 }
