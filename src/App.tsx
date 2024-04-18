46afc64976a0d5e668fce2007606cb8a20bcc693
eshansinghal14 <eshansinghal05@gmail.com>
2024-04-18 16:33:56 -0400

    new json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from "react";
+import data from './example.json';
 import "./App.css";
 import Hello from "./components/Hello";
 
 function App() {
   return (
     <div className="App">
-      <h1>Extension Name</h1>
+      <h1>Focus Tabs</h1>
       {/* Render the SnippetList component with the snippets and event handlers */}
-      <Hello person="Wod" />
+      <Hello person="World" />
+      <ul>
+        {data.tabs.map((group, index) => (
+          <li key={index}>
+            <button type="button" className="list-button">
+              {group.title}
+            </button>
+          </li>
+        ))}
+    </ul>
     </div>
   );
 }
