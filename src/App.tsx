c259d24098941a87dbbe2913494b4ddad32a6393
eshansinghal14 <eshansinghal05@gmail.com>
2024-04-25 16:57:22 -0400

    ui push

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from "react";
-import data from './example.json';
 import "./App.css";
 import Hello from "./components/Hello";
+import data from './example.json';
 
 function App() {
+  const [openDropdown, setOpenDropdown] = useState(null); // Track which dropdown is open
+  const toggleDropdown = (index) => {
+    if (openDropdown === index) { // If clicking on the currently open dropdown, close it
+      setOpenDropdown(null);
+    } else {
+      setOpenDropdown(index); // Otherwise, open the clicked dropdown
+    }
+  }
+  console.log(toggleDropdown);
   return (
     <div className="App">
       <h1>Focus Tabs</h1>
-      {/* Render the SnippetList component with the snippets and event handlers */}
       <Hello person="World" />
       <ul>
-        {data.tabs.map((group, index) => (
+        {data.groups.map((group, index) => (
           <li key={index}>
-            <button type="button" className="list-button">
-              {group.title}
-            </button>
+            <div className="dropdown">
+              <button onClick={() => toggleDropdown(index)} className="dropbtn">{group.name}</button>
+              <div className={openDropdown === index ? "dropdown-content show" : "dropdown-content"}>
+                {group.tabs.map((link, j) => (
+                  <a key={j} href={link.url}>{link.title}</a> // Fixed to dynamically display link titles
+                ))}
+              </div>
+            </div>
           </li>
         ))}
-    </ul>
+      </ul>
     </div>
   );
 }
